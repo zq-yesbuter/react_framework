75c6036fd546fc819ce125b31cd05320ce7cf529
zhangqing272 <zhangqing272@jd.com>
2019-12-27 14:17:59 +0800

    fix: 修改主题色

--- src/pages/AI/Resume/index.js
+++ src/pages/AI/Resume/index.js
@@ -69,7 +69,7 @@ function Resume({
   function header() {
     return (
       <div className={styles.header}>
-        <h3>{`简历来源：${channel === 'liepin' ? '猎聘网': '网络渠道'}`}</h3>
+        <h3>{`简历来源：${channel || '网络渠道'}`}</h3>
         <Button onClick={showResume}>查看原简历</Button>
       </div>
     );
